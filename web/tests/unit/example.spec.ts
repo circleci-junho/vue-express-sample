f3e0aa4acba2d1349003c79a9333d47d5e6b7abe
JUNHAENG HUR <pajums0235@gmail.com>
2022-06-15 16:04:59 +0900

    add-fizzbuzz-spec

--- web/tests/unit/example.spec.ts
+++ web/tests/unit/example.spec.ts
@@ -1,12 +1,54 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import FizzBuzz from '@/components/FizzBuzz.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('FizzBuzz result count', () => {
+  let wrapper: any;
+
+  beforeEach(async () => {
+    wrapper = shallowMount(FizzBuzz);
+  })
+
+  it('Result count is zero by default', () => {
+    expect(wrapper.vm.result.count).toBe(0)
+  })
+
+  it('Result count increase after click the button', async () => {
+    await wrapper.find('.fizzBuzz__button').trigger('click');
+    expect(wrapper.vm.result.count).toBe(1)
+  })
+})
+
+describe('FizzBuzz result fizzBuzz', () => {
+  let wrapper: any;
+
+  beforeEach(async () => {
+    wrapper = shallowMount(FizzBuzz);
+  })
+
+  it('Result fizzBuzz is empty by default', () => {
+    expect(wrapper.vm.result.fizzBuzz).toBe("")
+  })
+
+  it('Result fizzBuzz is Fizz when count is Multiple of 3', async () => {
+    wrapper.vm.result.count = 2
+    await wrapper.find('.fizzBuzz__button').trigger('click');
+    expect(wrapper.vm.result.fizzBuzz).toBe("Fizz")
+  })
+
+  it('Result fizzBuzz is Buzz when count is Multiple of 5', async () => {
+    wrapper.vm.result.count = 4
+    await wrapper.find('.fizzBuzz__button').trigger('click');
+    expect(wrapper.vm.result.fizzBuzz).toBe("Buzz")
+  })
+
+  it('Result fizzBuzz FizzBuzz when count is Multiple of 15', async () => {
+    wrapper.vm.result.count = 14
+    await wrapper.find('.fizzBuzz__button').trigger('click');
+    expect(wrapper.vm.result.fizzBuzz).toBe("FizzBuzz")
+  })
+
+  it('Result fizzBuzz is empty other case', async () => {
+    await wrapper.find('.fizzBuzz__button').trigger('click');
+    expect(wrapper.vm.result.count).toBe(1)
   })
 })
